a6f099b5497a6406671014bafe64fdfe1fa52244
Nabil <nabilalcha58@gmail.com>
2019-11-14 15:42:21 +0700

    Tugas 3 PHP
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Tugas3.php
@@ -0,0 +1,13 @@
+<?php
+$v = 90;
+$s = 360;
+$t = $s / $v;
+$t = $t*60;
+
+echo "<p>
+			Jarak Kota A - Z = $s Km <br>
+			Kecepatan Motor = $v <br>
+			Waktu Tempuh = ? <br>
+			Jawaban : $t Menit
+	  </p>";
+?>
---
+++ Tugas3_2.php
@@ -0,0 +1,13 @@
+<?php
+$tabunganAwal = 150000;
+$bunga = (12.5/100)*$tabunganAwal;
+$total = $tabunganAwal + $bunga;
+
+echo "<p>
+			Tabungan Awal = $tabunganAwal <br>
+			Bunga Setahun = 12.5% <br>
+			Total Tabungan Setahun = ? <br>
+			Jawaban : Rp$total
+	 </p>";
+ ?>
+
